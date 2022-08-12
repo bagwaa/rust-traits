bff871b806a468d2ae586daaaa893c73aa623945
Richard Bagshaw <me@richardbagshaw.co.uk>
2022-08-12 15:02:13 +0100

    useful commit message

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,23 @@
-use std::f64::consts::PI;
+use std::marker::Copy;
+use std::ops::Mul;
 
-// This is a bit like an interface in PHP, anything that implements this trait must provide an
-// implementation for the area method.
-trait HasArea {
-    fn area(&self) -> f64;
+struct Square<T> {
+    x: T,
+    y: T,
 }
 
-// Define a simple struct for a Square with a length and a width
-struct Square {
-    x: f64,
-    y: f64,
-}
-
-// Add a static method to the Square struct that we can use to construct a square
-impl Square {
-    fn new(x: f64, y: f64) -> Square {
+impl<T: Mul<Output = T> + Copy> Square<T> {
+    fn new(x: T, y: T) -> Square<T> {
         Square { x, y }
     }
-}
 
-// Implement the area() method for the Square
-impl HasArea for Square {
-    fn area(&self) -> f64 {
+    fn area(&self) -> T {
         self.x * self.y
     }
 }
 
-// Define a simple struct for a Circle with just a radius
-struct Circle {
-    r: f64,
-}
-
-// Add a static method to the Circle struct that we can use to construct a circle
-impl Circle {
-    fn new(r: f64) -> Circle {
-        Circle { r }
-    }
-}
-
-// Implement the area() method for the Circle
-impl HasArea for Circle {
-    fn area(&self) -> f64 {
-        PI * self.r * self.r
-    }
-}
-
 fn main() {
-    let square = Square::new(10_f64, 10_f64);
+    let square = Square::new(10.1, 10.2);
     println!(
         "The square is {} cm by {} cm and the area is {} cm",
         square.x,
@@ -54,11 +25,12 @@ fn main() {
         square.area()
     );
 
-    let circle = Circle::new(10_f64);
+    let square = Square::new(10, 12);
     println!(
-        "The circle has a radius of {} cm and an area of {:.2} cm",
-        circle.r,
-        circle.area()
+        "The square is {} cm by {} cm and the area is {} cm",
+        square.x,
+        square.y,
+        square.area()
     );
 }
 
